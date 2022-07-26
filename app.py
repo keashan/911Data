5226855aeef212a3e54b0f8333aefcaa30926be9
Keashan Jayaweera <keashanjayaweera@gmail.com>
2022-07-26 16:36:17 +0530

    postgrey sql link removed

--- app.py
+++ app.py
@@ -19,10 +19,3 @@ server = app.server
 
 app.config.suppress_callback_exceptions = True
 
-# config
-server.config.update(
-    SECRET_KEY=os.urandom(12),
-    SQLALCHEMY_DATABASE_URI='postgresql://soknmkqscvzrmw:1dbfd508eb100989cba47535362bd54d93a5607f05f558b8952ae2f20515521d@ec2-52-209-171-51.eu-west-1.compute.amazonaws.com:5432/d7vpl3clbr272v',
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-)
-
