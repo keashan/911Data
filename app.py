ad03b4755d19136e83c47e7965ef676e7308a4fd
Keashan Jayaweera <keashanjayaweera@gmail.com>
2023-01-25 02:12:06 +0530

    bug fix

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from data_processing.get_data import save_pickle
 
 # Initialize the app
 
-server = Flask(__name__)
+#server = Flask(__name__)
 
-app = dash.Dash(server=server,
+app = dash.Dash(__name__,
                 meta_tags=[{'name': 'viewport', 'content': "width=device-width, initial-scale=1.0"}],
                 external_stylesheets=[dbc.themes.LUX], title="911 Data Analysis")
 # cache = Cache(app.server, config={
